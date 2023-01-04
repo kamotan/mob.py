ac507927275f073347e46df81e64d4f34cb07819
kamotan <kamotan.kamotan3@gmail.com>
2023-01-04 15:10:30 +0900

    csv

--- test_py.py
+++ test_py.py
@@ -10,9 +10,11 @@ import datetime
 import cv2 
 import random
 import numpy as np
+import csv
+import pprint
+import os
 
-
-TOKEN = 'M TA0Mjc2MDI1MjAyNzgzNDQxOA.G4CgvH.DRZqaeSU0WHJ_GHoNaJ5kFfPbLqgLQNh-axmqA'
+TOKEN = 'MTA0Mjc2MDI1MjAyNzgzNDQxOA.G4CgvH.DRZqaeSU0WHJ_GHoNaJ5kFfPbLqgLQNh-axmqA'
 CHANNEL_ID = 1042758547089395753
 client = discord.Client()
 time = datetime.datetime.now()
@@ -20,107 +22,92 @@ s = '〜〜〜'
 
 @client.event
 async def on_ready():
-    print('Bot Launched')
-    print(time)
-    print(client.get_channel)
+   print('Bot Launched')
+   print(time)
+   print(client.get_channel)
 @client.event
 async def on_message(message):
-    if message.author.bot:
+   if message.author.bot:
         return
    
    #DM
-    if message.channel.id != CHANNEL_ID:
-        if ('！BOT呼び出し') in message.content:
-           channel = message.channel
-           await channel.send("こんにちは！育成しますか？ストーリーを作りますか？\n育成/ストーリー作成")
-           def check(m):
-            # メッセージが `おはよう` かつ メッセージを送信したチャンネルが
-            # コマンドを打ったチャンネルという条件
-            return m.content == '育成' or m.content == 'ストーリー作成' and m.channel == channel
-           try:
-              # wait_forを用いて、イベントが発火し指定した条件を満たすまで待機する
-              msg = await client.wait_for('message', check=check, timeout=30)
-              # wait_forの1つ目のパラメータは、イベント名の on_がないもの
-              # 2つ目は、待っているものに該当するかを確認する関数 (任意)
-              # 3つ目は、タイムアウトして asyncio.TimeoutError が発生するまでの秒数
-            
-          # asyncio.TimeoutError が発生したらここに飛ぶ
-          except asyncio.TimeoutError:
-              await channel.send('タイムアウトしました')
-          else:
-             if ('育成') in message.content:
-           
-             if ('ストーリー作成') in message.content:
-                await channel.send('タイトルを入力してください')
-                def check(m):
-            
-                return m.channel == channel
-                try:
-                   msg = await client.wait_for('message', check=check, timeout=30)
-                except asyncio.TimeoutError:
-                   await channel.send('タイムアウトしました')
-                else:
-                   #タイトルをtxtに代入
-                   title = message
-                   os.mkdir(title)
-                   path_1 = '{}/title.txt'.format(title)
-                   with open(path_1, mode='w') as f:
-                        f.write(s)
-                   with open(path_1, mode='a') as f:
-                        f.write('\n')
-                        f.write(text)
-                        f.write('〜〜〜')
+   if message.channel.id != CHANNEL_ID:
+      if ('！BOT呼び出し') in message.content or ('!BOT呼び出し') in message.content:
+            channel = message.channel
+            await channel.send("こんにちは！育成しますか？ストーリーを作りますか？\n育成/ストーリー作成\n▼")
+            def check(a):
+               return a.content == '育成' or a.content == 'ストーリー作成' and a.channel == channel
+            msg = await client.wait_for('message', check=check,timeout=120)
+            if ('育成') in message.content:
+               await channel.send(f'Hello {msg.author}!')
+               with open('a.csv') as f:
+                  reader = csv.reader(f)
+                  l = [row for row in reader]
+                  
+               with open('a.csv', 'a') as f:
+                  writer = csv.writer(f)
+                  writer.writerow([message])
+                  # print(l[1])
+                  # ['21', '22', '23', '24']
+                  # print(l[1][1])
+                  # 22
+
+            elif ('ストーリー作成') in message.content:
+               await channel.send('タイトルを入力してください\n▼') 
+               def check(a):
+                  return a.channel == channel
+               msg = await client.wait_for('message', check=check,timeout=120)
+               title = message
+               await channel.send("育成ストーリー紹介文を入力してください\n▼")
+               def check(a):
+                  return a.channel == channel
+               msg = await client.wait_for('message', check=check,timeout=120)
+               introduction = message
+               await channel.send("冒頭イベントを入力してください\n▼")
+               def check(a):
+                  return a.channel == channel
+               msg = await client.wait_for('message', check=check,timeout=120)
+               opening = message
+               
+               await channel.send("次のイベントまで何ターン空きを作りますか。\nターン数を半角で打ち込んでください。\nもうこれで終わりの場合は「やめる」と入力してください。\n▼")
+               def check(a):
+                  return a.channel == channel
+               msg = await client.wait_for('message', check=check,timeout=120)
+               if ('やめる') in message.content:
+                  await channel.send(f'Hello {msg.author}!')
+               else:   
+                  turn1 = message
+                  await channel.send("次のイベントは通常ですか？レースですか？\n通常/レース\n▼")
+                  def check(a):
+                     return a.channel == channel
+                  msg = await client.wait_for('message', check=check,timeout=120)
+                  if ('通常') in message.content:
+                     normal = message
+                     await channel.send("イベント内容を入力してください。\nもう入力しない場合は「やめる」と入力してください。\n▼")
+                     def check(a):
+                        return a.channel == channel
+                     msg = await client.wait_for('message', check=check,timeout=120)
+                     if ('やめる') in message.content:
+                        await channel.send(f'Hello {msg.author}!')
+                     else:
+                        normal = message
+                        await channel.send("イベント内容を入力してください。\nもう入力しない場合は「やめる」と入力してください。\n▼") 
+                        await channel.send(f'Hello {msg.author}!')
+
+                  elif ('レース') in message.content:
+                     race = message
+                     await channel.send(f'Hello {msg.author}!')
+                     
+                  
+              
+                  
                  
-                   await channel.send('育成ストーリー紹介文を入力してください')
-                   def check(m):
-            
-                   return m.channel == channel
-                   try:
-                      msg = await client.wait_for('message', check=check, timeout=30)
-                   except asyncio.TimeoutError:
-                      await channel.send('タイムアウトしました')
-                   else:
-                      #txtに代入
-                      Introduction = message
-                      os.mkdir(Introduction)
-                      path_2 = '{}/Introduction.txt'.format(title)
-                      with open(path_2, mode='w') as f:
-                           f.write(s)
-                      with open(path_2, mode='a') as f:
-                           f.write('\n')
-                           f.write(Introduction)
-                           f.write('〜〜〜')
-                 
-                      await channel.send('冒頭イベントを入力してください')
-                      def check(m):
-            
-                      return m.channel == channel
-                      try:
-                        msg = await client.wait_for('message', check=check, timeout=30)
-                      except asyncio.TimeoutError:
-                        await channel.send('タイムアウトしました')
-                      else:
-                        #txtに代入
-                        opening = message
-                        os.mkdir(opening)
-                        path_3 = '{}/opening.txt'.format(title)
-                        with open(path_3, mode='w') as f:
-                           f.write(s)
-                        with open(path_3, mode='a') as f:
-                           f.write('\n')
-                           f.write(opening)
-                           f.write('〜〜〜')
-                 
-                        await channel.send('冒頭イベントを入力してください')
+                   
                   
      
    #サーバー  
-    if ('１０連') in message.content:
-
-
-    
-
-    print("1")
+   if ('１０連') in message.content:
+      print("1")
     
         
     
